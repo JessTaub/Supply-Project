55fda00dfaa4388e37afb2d094a53b56356f966b
Jess Tauber <jester891@gmail.com>
2015-08-06 17:34:59 -0500

    got read.table to work for this

--- supply.r
+++ supply.r
@@ -3,5 +3,9 @@ library(foreign)
 #Read in supply chain data
 banana.data <- read.csv('C:/Users/Grant/Dropbox/supply/Supply Chain Data/banana_republic.csv', header= TRUE, skip=5)
 
+banana.df=read.table(file.choose(),header=TRUE,sep=",",fill=TRUE) #fill=TRUE fixed problem where R couldn't handle a blank space in a dataframe
+
 #View data
 View(banana.data)
+
+#E:\HDocuments\Supply Project
