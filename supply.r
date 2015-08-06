bbaa291a92a0c892e98a68de05332d6c46a07bb9
Jess Tauber <jester891@gmail.com>
2015-08-06 17:18:36 -0500

    trying this out

---
+++ supply.r
@@ -0,0 +1,7 @@
+library(foreign)
+
+#Read in supply chain data
+banana.data <- read.csv('C:/Users/Grant/Dropbox/supply/Supply Chain Data/banana_republic.csv', header= TRUE, skip=5)
+
+#View data
+View(banana.data)
