8185812012a924705135d12d5aba10a6c32b0b6a
gkopplin <grant.kopplin@gmail.com>
2015-08-13 23:19:13 -0400

    Revised Loop on Grepl
    I think I got it! I still need to check it though

--- supply.r
+++ supply.r
@@ -1,12 +1,11 @@
 library(foreign)
 
 #Read in supply chain data
-banana.data <- read.csv('C:/Users/Grant/Dropbox/supply/Supply Chain Data/banana_republic.csv', header= TRUE, skip=5)
+banana.data <- read.csv('C:/Users/Grant/Dropbox/supply/Supply Chain Data/banana_republic_raw.csv', header= TRUE, skip=5)
 
 banana.df=read.table(file.choose(),header=TRUE,sep=",",fill=TRUE) #fill=TRUE fixed problem where R couldn't handle a blank space in a dataframe
-
-#View data
-View(banana.data)
+#GK-08-13-15
+#This table has 5 more obs. whats up with that?
 
 #E:\HDocuments\Supply Project
 
@@ -20,3 +19,10 @@ for (i in 1:2){
   dummy[i] <- grepl("ADD",var[i])
 }
 dummy
+
+#GK-08-13-15
+#revised loop on grepl
+app <- apply(banana.data, 2, function(x) grepl("BANANA REPUBLIC", x))
+
+banana.sub <- subset(banana.data, apply(app, 1, function(x) any(x==T)))
+
